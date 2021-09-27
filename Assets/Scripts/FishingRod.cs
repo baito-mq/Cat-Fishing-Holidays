052a920581209170c7fa80e599eeb1172f9cd50e
Anky Y <32216729+ratquaza@users.noreply.github.com>
2021-09-27 21:59:11 +1000

    fishing rod script

---
+++ Assets/Scripts/FishingRod.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingRod : MonoBehaviour
+{
+    public BoxCollider WaterChecker;
+    public LayerMask WaterLayer;
+    public ThirdPersonMovement PlayerMovement;
+
+    private bool IsFishing = false;
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fish"))
+        {
+            if (!IsFishing)
+            {
+                bool value = Physics.CheckBox(WaterChecker.transform.position, WaterChecker.bounds.extents, WaterChecker.transform.rotation, WaterLayer);
+                if (value)
+                {
+                    StartFishing();
+                }
+            } else
+            {
+                StopFishing();
+            }
+        }
+    }
+
+    public void StartFishing()
+    {
+        IsFishing = true;
+        PlayerMovement.enabled = false;
+    }
+
+    public void StopFishing()
+    {
+        IsFishing = false;
+        PlayerMovement.enabled = true;
+    }
+}
