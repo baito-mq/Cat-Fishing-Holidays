68075d083e98ecc578ef91e469f52bbe98f7494c
aatroxgod <tianheng.zhao@students.mq.edu.au>
2021-10-28 00:04:02 +0800

    end game function started

--- Assets/Scripts/FishingRod.cs
+++ Assets/Scripts/FishingRod.cs
@@ -9,27 +9,37 @@ public class FishingRod : MonoBehaviour
     public ThirdPersonMovement PlayerMovement;
     public GameObject Camera;
     public GameObject RhythmGamePrefab;
-
+private GameObject clone;
+private Vector3 Cam1= new Vector3(11,6,-14);
     private bool IsFishing = false;
 
     public void Update()
     {
-        if (Input.GetButtonDown("Fish"))
+        if (Input.GetButtonDown("Fish"))// press E 
         {
-            if (!IsFishing)
-            {
+            if (!IsFishing)// if it is not fishing
+            {//check if water is nearby 
                 bool value = Physics.CheckBox(WaterChecker.transform.position, WaterChecker.bounds.extents, WaterChecker.transform.rotation, WaterLayer);
-                if (value)
+                if (value)//true
                 {
                     StartFishing();
                 }
-            } else
+            } else//false
             {
                 StopFishing();
             }
         }
+		if(Input.GetButtonDown("End"))
+	{
+		if(IsFishing==true){
+		
+		Camera.transform.position= Cam1;//11,6,-14, 
+		Camera.transform.rotation= Quaternion.Euler(17,0,0);
+		StopFishing();
+		Destroy(clone);
+	}
     }
-
+	}
     public void StartFishing()
     {
         IsFishing = true;
@@ -41,12 +51,18 @@ public class FishingRod : MonoBehaviour
   RhythmManager Anky=clone.GetComponent<RhythmManager>();
   Anky.NoteSpeed= 400f;
   Anky.SpawnRandom();
+Destroy(clone,15);
 
     }
+	
 
+
+	
     public void StopFishing()
     {
         IsFishing = false;
         PlayerMovement.enabled = true;
+		
+		
     }
 }
