aca37efe6d330c1c29c42e2f9d60066f79b121d5
aatroxgod <tianheng.zhao@students.mq.edu.au>
2021-11-04 10:47:56 +0800

    debug done

--- Assets/Scripts/AudioRan.cs
+++ Assets/Scripts/AudioRan.cs
@@ -17,13 +17,35 @@ public class AudioRan
     // Update is called once per frame
     void Update (){
         audioSource.volume = musicVolume;
-        if ((state == 1 && !audioSource.isPlaying)||(state == 2 && !audioSource.isPlaying) ||(state == 3 && !audioSource.isPlaying)) { randomPlay(); }
+        if (state == 1 && !audioSource.isPlaying)
+		{audioSource.clip = otherClip1; 
+		audioSource.Play();}
+		else if
+		(state == 2 && !audioSource.isPlaying){
+		audioSource.clip = otherClip2; 
+		audioSource.Play();
+		}
+		else if(state == 3 && !audioSource.isPlaying)
+		{ audioSource.clip = otherClip3; 
+		audioSource.Play(); }
     }
+ 
     void randomPlay()
     {
         randomNum = Random.Range(1.0f, 4.0f);
-        if (randomNum >= 1.0f && randomNum < 2.0f) { state = 1; audioSource.clip = otherClip1; audioSource.Play(); }
-        else if (randomNum >= 2.0f && randomNum < 3.0f) { state = 2; audioSource.clip = otherClip2; audioSource.Play(); }
-        else if (randomNum >= 3.0f && randomNum <= 4.0f) { state = 3; audioSource.clip = otherClip3; audioSource.Play(); }
+        if (randomNum >= 1.0f && randomNum < 2.0f)
+			{ state = 1;
+		}
+        else if (randomNum >= 2.0f && randomNum < 3.0f) {
+			state = 2; 
+			
+			
+			 }
+        else if (randomNum >= 3.0f && randomNum <= 4.0f) {
+			state = 3; 
+		}
+			
+		 
+			
     }
 }
